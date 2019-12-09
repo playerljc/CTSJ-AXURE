a1d1f724f6e32d50e04cce7cd8a3a38e39dbfd33
zhiqian.liu <zhiqian.liu@baifendian.com>
2019-12-09 15:46:22 +0800

    RangeSelect拆分

--- src/components/library/component/RangeSelectKeyBoard.js
+++ src/components/library/component/RangeSelectKeyBoard.js
@@ -76,6 +76,9 @@ class RangeSelectKeyBoard {
     ]);
   }
 
+  /**
+   * bindKeyBoard
+   */
   bindKeyBoard() {
     const entrys = this.keyBoardMap.entries();
     for (const [key, handler] of entrys) {
@@ -93,101 +96,161 @@ class RangeSelectKeyBoard {
     }
   }
 
+  /**
+   * onArrowUp
+   */
   onArrowUp() {
     console.log('arrowUp');
     this[rangeSelectIns].arrowDetail('top');
   }
 
+  /**
+   * onArrowDown
+   */
   onArrowDown() {
     console.log('arrowDown');
     this[rangeSelectIns].arrowDetail('bottom');
   }
 
+  /**
+   * onArrowLeft
+   */
   onArrowLeft() {
     console.log('arrowLeft');
     this[rangeSelectIns].arrowDetail('left');
   }
 
+  /**
+   * onArrowRight
+   */
   onArrowRight() {
     console.log('arrowRight');
     this[rangeSelectIns].arrowDetail('right');
   }
 
+  /**
+   * onCtrlArrowUp
+   */
   onCtrlArrowUp() {
     console.log('ctrlArrowUp');
     this[rangeSelectIns].arrowDetail('top', KEYBOARD_FAST_STEP);
   }
 
+  /**
+   * onCtrlArrowDown
+   */
   onCtrlArrowDown() {
     console.log('ctrlArrowDown');
     this[rangeSelectIns].arrowDetail('bottom', KEYBOARD_FAST_STEP);
   }
 
+  /**
+   * onCtrlArrowLeft
+   */
   onCtrlArrowLeft() {
     console.log('ctrlArrowLeft');
     this[rangeSelectIns].arrowDetail('left', KEYBOARD_FAST_STEP);
   }
 
+  /**
+   * onCtrlArrowRight
+   */
   onCtrlArrowRight() {
     console.log('ctrlArrowRight');
     this[rangeSelectIns].arrowDetail('right', KEYBOARD_FAST_STEP);
   }
 
+  /**
+   * onRepeatArrowUp
+   */
   onRepeatArrowUp() {
     console.log('repeatArrowUp');
     this[rangeSelectIns].arrowDetail('top');
   }
 
+  /**
+   * onRepeatArrowDown
+   */
   onRepeatArrowDown() {
     console.log('repeatArrowDown');
     this[rangeSelectIns].arrowDetail('bottom');
   }
 
+  /**
+   * onRepeatArrowLeft
+   */
   onRepeatArrowLeft() {
     console.log('repeatArrowLeft');
     this[rangeSelectIns].arrowDetail('left');
   }
 
+  /**
+   * onRepeatArrowRight
+   */
   onRepeatArrowRight() {
     console.log('repeatArrowRight');
     this[rangeSelectIns].arrowDetail('right');
   }
 
+  /**
+   * onRepeatCtrlArrowUp
+   */
   onRepeatCtrlArrowUp() {
     console.log('repeatCtrlArrowUp');
     this[rangeSelectIns].arrowDetail('top', KEYBOARD_FAST_STEP);
   }
 
+  /**
+   * onRepeatCtrlArrowDown
+   */
   onRepeatCtrlArrowDown() {
     console.log('repeatCtrlArrowDown');
     this[rangeSelectIns].arrowDetail('bottom', KEYBOARD_FAST_STEP);
   }
 
+  /**
+   * onRepeatCtrlArrowLeft
+   */
   onRepeatCtrlArrowLeft() {
     console.log('repeatCtrlArrowLeft');
     this[rangeSelectIns].arrowDetail('left', KEYBOARD_FAST_STEP);
   }
 
+  /**
+   * onRepeatCtrlArrowRight
+   */
   onRepeatCtrlArrowRight() {
     console.log('repeatCtrlArrowRight');
     this[rangeSelectIns].arrowDetail('right', KEYBOARD_FAST_STEP);
   }
 
+  /**
+   * onCtrlC
+   */
   onCtrlC() {
     console.log('CtrlC');
     this[rangeSelectIns].copy();
   }
 
+  /**
+   * onDelete
+   */
   onDelete() {
     console.log('Delete');
     this[rangeSelectIns].deleteSelf();
   }
 
+  /**
+   * onBackapace
+   */
   onBackapace() {
     console.log('Backapace');
     this[rangeSelectIns].deleteSelf();
   }
 
+  /**
+   * onCtrl
+   */
   onCtrl() {
     console.log('Ctrl');
   }
